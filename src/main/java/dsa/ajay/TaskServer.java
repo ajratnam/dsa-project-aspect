e65d4b472fc5861cb964d63a3b5a7b130d45d6cf
Ajay Ratnam <ajratnam.dev@gmail.com>
2024-05-23 16:24:05 +0530

    Multi Client

--- src/main/java/dsa/ajay/TaskServer.java
+++ src/main/java/dsa/ajay/TaskServer.java
@@ -15,29 +15,34 @@ public class TaskServer {
         try (ServerSocket serverSocket = new ServerSocket(12345)) {
             System.out.println("Server is listening on port 12345");
 
-            while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    System.out.println("Client connected");
+            int clientCount = 0;
+            Socket[] clients = new Socket[3];
+            ObjectOutputStream[] clientStreams = new ObjectOutputStream[3];
 
-                    // Create an instance of the target object (e.g., GlobalContext)
-                    GlobalContext context = new GlobalContext(10);
-                    GlobalContext targetObject = (GlobalContext) ObjectGenerator.generateTargetObject(context);
+            while (clientCount < 3) {
+                clients[clientCount] = serverSocket.accept();
+                clientStreams[clientCount] = new ObjectOutputStream(clients[clientCount].getOutputStream());
+                System.out.println("Client connected");
+                clientCount++;
+            }
 
-                    // Now, whenever you call a method on targetObject, a GenericTask will be automatically created and added to tasks
-                    targetObject.performOperation(5, "Test");
-                    targetObject.anotherMethod("Hello");
-                    targetObject.yetAnotherMethod();
+            // Create an instance of the target object (e.g., GlobalContext)
+            GlobalContext context = new GlobalContext(10);
+            GlobalContext targetObject = (GlobalContext) ObjectGenerator.generateTargetObject(context);
 
-                    // Get the tasks from the context
-                    List<GenericTask> tasks = context.getTasks();
+            // Now, whenever you call a method on targetObject, a GenericTask will be automatically created and added to tasks
+            targetObject.performOperation(5, "Test");
+            targetObject.anotherMethod("Hello");
+            targetObject.yetAnotherMethod();
 
-                    // Send the tasks
-                    try (ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream())) {
-                        oos.writeObject(tasks);
-                        oos.flush();
-                        System.out.println("Tasks sent to client");
-                    }
-                }
+            // Get the tasks from the context
+            List<GenericTask> tasks = context.getTasks();
+
+            // Send the tasks
+            for (int i = 0; i < 3; i++) {
+                clientStreams[i].writeObject(List.of(tasks.get(i)));
+                clientStreams[i].flush();
+                System.out.println("Task sent to client " + (i + 1));
             }
         } catch (Exception e) {
             e.printStackTrace();
